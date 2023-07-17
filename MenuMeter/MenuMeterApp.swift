4c35b30ad9da1ae68673f3ffa5316cd1ff37d005
rogpires <rogeriocpires@hotmail.com>
2023-07-17 20:38:02 -0300

    Initial Commit

---
+++ MenuMeter/MenuMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuMeterApp.swift
+//  MenuMeter
+//
+//  Created by Rogerio Pires on 17/07/23.
+//
+
+import SwiftUI
+
+@main
+struct MenuMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
